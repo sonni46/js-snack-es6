12185d886ad39e3ff7e0a09997933054e1cf2705
Marco <sonni46marcosenpai@gmail.com>
2021-09-01 17:00:55 +0200

    fine con bug

--- jsnacks3/js/main.js
+++ jsnacks3/js/main.js
@@ -3,23 +3,24 @@
 // La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri.
 // Usiamo i nuovi metodi degli array foreach o filter.
 const myArray = [1,2,3,4,5,6,7,8,9,10];
-let a = random(1, 10);
-let b = random(1, 10);
+let a = parseInt(random(1, 10)); 
+let b = parseInt(random(1, 10)); 
+
+console.log(`${a} e ${b}`);
+
 const myFuction = (array,a,b) => {
     if (a < b) {
-        return true;
-    }
-    const newArray = array.filter((element,index,) => { 
-    if (index >= a && index <= b ){
-        return true
-    }
-    return false   
-    } );
+        const newArray = array.filter((element,index,) => { 
+        if (index >= a && index <= b ) return true;  
+        return false;    
+    });
+    return newArray;   
+}
 }
 
-myFuction(myArray,a,b);
 
-console.log(newArray);
+
+console.log(myFuction(myArray,a,b))
 
 // function// function// function
 function random(min, max) {
